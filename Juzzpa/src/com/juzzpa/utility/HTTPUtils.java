73bce764202bfd2d0571c8e0cd127e5aa4c46988
Bharat Gudihal <bharat.gudi@gmail.com>
2015-08-18 21:48:49 +0530

    Removed dependency on app engine jars from httputils

--- Juzzpa/src/com/juzzpa/utility/HTTPUtils.java
+++ Juzzpa/src/com/juzzpa/utility/HTTPUtils.java
@@ -12,9 +12,8 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.HttpClientBuilder;
-
-import com.google.appengine.labs.repackaged.org.json.JSONException;
-import com.google.appengine.labs.repackaged.org.json.JSONObject;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * @author Bharat
@@ -24,15 +23,18 @@ public class HTTPUtils {
 
 	private static HttpClient httpClient = HttpClientBuilder.create().build();
 
-	public static HttpResponse postRequest(HttpPost post) throws ClientProtocolException, IOException{
+	public static HttpResponse postRequest(HttpPost post)
+			throws ClientProtocolException, IOException {
 		return httpClient.execute(post);
 	}
 
-	public static JSONObject extractResponse(HttpResponse response) throws IllegalStateException, IOException, JSONException {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+	public static org.json.JSONObject extractResponse(HttpResponse response)
+			throws IllegalStateException, IOException, JSONException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(
+				response.getEntity().getContent()));
 		String line;
 		StringBuilder sb = new StringBuilder();
-		while(null != (line = reader.readLine())){
+		while (null != (line = reader.readLine())) {
 			sb.append(line);
 		}
 		JSONObject jsonObject = new JSONObject(sb.toString());
